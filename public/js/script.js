48a318e1221508b5caa08d691861c8b5bda2f6bd
IsmailZaman <IsmailZaman70@gmail.com>
2021-07-23 21:48:16 +0500

    setting up heroku app

--- public/js/script.js
+++ public/js/script.js
@@ -18,7 +18,7 @@ toggleJokeBtn();
 
 function displayJoke(){
 
-    const url = new URL("http://localhost:3000/joke");
+    const url = "/joke";
     fetch(url).then((response)=>{
         response.json().then((data)=>{
 
